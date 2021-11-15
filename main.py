744e9dde44b0cc65a9dea29c863ac8117a2e28e5
Klay Clarke <klayaclarke@gmail.com>
2021-11-15 17:11:08 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape('turtle')
+timmy_the_turtle.color('burlywood')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
